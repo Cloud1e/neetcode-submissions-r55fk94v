3ee49f34383e408e662413d4f716d90a7d696cbd
Chenbo Yin <56658249+Cloud1e@users.noreply.github.com>
2026-04-07 21:28:30 -0500

    Update: valid-sudoku - submission-2

--- Data Structures & Algorithms/valid-sudoku/submission-2.py
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [0] * 9
-        cols = [0] * 9
-        squares = [0] * 9
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
         m, n = len(board), len(board[0])
         for r in range(m):
             for c in range(n):
                 if board[r][c].isdigit():
                     num = int(board[r][c])
-                    if (rows[r] >> num) & 1 or (cols[c] >> num) & 1 or (squares[r // 3 * 3 + c // 3] >> num) & 1:
-                        return False                
-                    rows[r] |= (1 << num) 
-                    cols[c] |= (1 << num)
-                    squares[r // 3 * 3 + c // 3] |= (1 << num)  
+                    if num in rows[r] or num in cols[c] or num in squares[r // 3 * 3 + c // 3]:
+                        return False
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    squares[r // 3 * 3 + c // 3].add(num)
         return True
